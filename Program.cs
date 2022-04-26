b3a4178f83dbe285c0bac937b145d0d5ce35136d
Gustavo Gonzalez <gusgonzalez.gg@gmail.com>
2022-04-26 19:43:39 -0400

    added Exercise 1 to repo)

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace IterationsStatementExercise42622
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            //for (int i =0; i <5; i++)
+            //{
+            //    Console.WriteLine($"{i}");
+            //}
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //Console.WriteLine($"{i}");
+            //}
+
+            //for (int i =0; i <5; i++)
+            //{
+            //    Console.WriteLine($"{i}");
+            //}
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //Console.WriteLine($"{i}");
+            //}//for (int i =0; i <5; i++)
+            //{
+            //    Console.WriteLine($"{i}");
+            //}
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //Console.WriteLine($"{i}");
+            //}//for (int i =0; i <5; i++)
+            //{
+            //    Console.WriteLine($"{i}");
+            //}
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //Console.WriteLine($"{i}");
+            //}//for (int i =0; i <5; i++)
+            //{
+            //    Console.WriteLine($"{i}");
+            //}
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //Console.WriteLine($"{i}");
+            //}//for (int i =0; i <5; i++)
+            //{
+            //    Console.WriteLine($"{i}");
+            //}
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //Console.WriteLine($"{i}");
+            //}//for (int i =0; i <5; i++)
+            //{
+            //    Console.WriteLine($"{i}");
+            //}
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //Console.WriteLine($"{i}");
+            //}
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //Console.WriteLine($"{i}");
+            //}
+        }
+    }
+}
